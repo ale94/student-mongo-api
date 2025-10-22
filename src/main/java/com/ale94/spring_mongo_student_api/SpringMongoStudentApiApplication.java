72ec6463e1b193f50a731be7420f3f787eb031d5
ale94 <alejandrorua111@gmail.com>
2025-10-21 23:18:04 -0300

    init: initial commit

---
+++ src/main/java/com/ale94/spring_mongo_student_api/SpringMongoStudentApiApplication.java
@@ -0,0 +1,13 @@
+package com.ale94.spring_mongo_student_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoStudentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoStudentApiApplication.class, args);
+	}
+
+}
